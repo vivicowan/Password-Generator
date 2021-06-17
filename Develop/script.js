2ed803656d04f0c23f30c4f5e9d42471bee1a082
Vivi Cowan <vivicare@gmail.com>
2021-06-17 16:59:01 -0700

    created variables for password options

--- Develop/script.js
+++ Develop/script.js
@@ -1,13 +1,56 @@
 // Assignment Code
 var generateBtn = document.querySelector('#generate');
+var passLength = 0;
+var useUpper = true;
+var useLower = true; 
+var useNum = true; 
+var useSpec = true;
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
   var passwordText = document.querySelector('#password');
-
+  
+  
   passwordText.value = password;
 }
 
+function generatePassword(){
+  alert ("Generate Password")
+}
+
+function promptCriteria () {
+  passLength = parseInt (window.prompt("Input between 8 and 128 characters"));
+  if(isNaN(passLength) || passLength < 8 || passLength > 128){
+    alert ("INVALID PASSWORD LENGTH");
+    passLength = 0;
+  } 
+  
+  
+
+
+}
+
+// var password = generatePassword();
+
+
 // Add event listener to generate button
-generateBtn.addEventListener('click', writePassword);
+generateBtn.addEventListener('click', promptCriteria);
+
+// declare some variables for the answers
+// password (the generated password)
+// allUpperCase (array of all uppercase letters)
+// allLowerCase (array of all lowercase letters)
+// allNumbers (array of all numbers)
+// allSpec (array of all special characters)
+// candidates (array of characters chosen by user)
+// passLength <- prompt for length
+// useUpper <- confirm use uppercase
+// useLower <- confirm use lowercase
+// useNum <- confirm use numbers
+// useSpec <- confirm use special chars
+// IF useUpper AND useLower AND useNum AND useSpec are ALL false
+//   alert "Choose at least one type of character."
+//   END function
+// IF passLength < 8 OR passLength > 128
+//   alert "Password length must be between 8 and 128 characters."
+//   END function
